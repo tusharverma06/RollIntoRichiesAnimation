ef2f30be9505f05b4d3e554bd3699e364349f1cd
tushar <117444846+tusharverma06@users.noreply.github.com>
2023-12-17 01:45:44 +0530

    added popin animation to journey video

--- src/Components/AboutPhoenix/AboutPhoenixSection.jsx
+++ src/Components/AboutPhoenix/AboutPhoenixSection.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Juice from '../../assets/About/juice.svg'
 import Cards from '../../assets/About/cards.svg'
 import Dices from '../../assets/About/dices.svg'
 import Trophy from '../../assets/About/trophy.svg'
 import SectionContainer from '../Container/SectionContainer'
+import { motion, useScroll, useTransform } from 'framer-motion'
+import { useMediaQuery } from '../../hooks/useMediaQuery'
+
 const AboutPhoenixSection = () => {
 
     const Info = [
@@ -32,6 +35,17 @@ const AboutPhoenixSection = () => {
         },
 
     ]
+    const targetRef = useRef(null)
+    const { scrollYProgress } = useScroll({
+        target: targetRef,
+        offset: ['start end', 'end start'],
+    })
+
+    const windoWidth = useMediaQuery()
+
+    const top = useTransform(scrollYProgress, [0, 0.16, 0.18], windoWidth < 600 ? ['60px', '60px', 'opx'] : ['60px', '60px', '0px'])
+    const scaleText = useTransform(scrollYProgress, [0, 0.16, 0.18], windoWidth < 600 ? ['0.9', '0.9', '1'] : ['0.7', '0.6', '1'])
+
 
     return (
         <SectionContainer
@@ -40,9 +54,11 @@ const AboutPhoenixSection = () => {
             }
         >
 
-            <div className="flex flex-wrap items-center justify-center w-full gap-y-10 md:justify-start lg:justify-between 3xl:justify-center 3xl:gap-24">
+            <div ref={targetRef} className="flex flex-wrap items-center justify-center w-full gap-y-10 md:justify-start lg:justify-between 3xl:justify-center 3xl:gap-24">
                 {Info.map(({ title, text, img }, id) =>
-                    <div key={id} className="flex flex-col font-Cinzel lg:w-[calc(100%/4-40px)] md:w-[calc(100%/2-32px)] w-full items-center justify-start max-w-[300px] gap-5 px-2 ">
+                    <motion.div
+                        style={{ y: top, scale: scaleText, }}
+                        key={id} className="flex transition-all duration-300 ease-linear flex-col font-Cinzel lg:w-[calc(100%/4-40px)] md:w-[calc(100%/2-32px)] w-full items-center justify-start max-w-[300px] gap-5 px-2 ">
                         {/* img,heading */}
                         <div className="flex flex-col items-center justify-start w-full gap-4">
                             <img src={img} alt='' />
@@ -52,7 +68,7 @@ const AboutPhoenixSection = () => {
                         <h4 className="w-full max-w-full text-[#ABABAB] text-sm lg:text-base text-center">
                             {text}
                         </h4>
-                    </div>
+                    </motion.div>
 
                 )}
             </div>
